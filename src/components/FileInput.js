8034c8f69f247f6f02a980f95f19667d93328578
minbo <smb5174@naver.com>
2024-02-07 22:54:58 +0900

    feat: file input 초기화

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -8,7 +8,20 @@ function FileInput({ name, value, onChange }) {
     onChange(name, nextValue);
   };
 
-  return <input type="file" onChange={handleChange} ref={inputRef} />;
+  const handleClearClick = () => {
+    const inputNode = inputRef.current;
+    if (!inputNode) return;
+
+    inputNode.value = "";
+    onChange(name, null);
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleChange} ref={inputRef} />
+      {value && <button onClick={handleClearClick}>X</button>}
+    </div>
+  );
 }
 
 export default FileInput;
